2e030232864ac0f2d04a16f0191fca9192891424
dragonwong <whileking@gmail.com>
2015-05-11 15:53:26 +0800

    修正 radio check

--- avalon.js
+++ avalon.js
@@ -2331,11 +2331,12 @@ var duplexBinding = bindingHandlers.duplex = function (data, vmodels) {
             elem.msData["ms-duplex"] = data.value
         }
         data.param.replace(/\w+/g, function (name) {
-            if (/^(checkbox|radio)$/.test(elem.type) && /^(radio|checked)$/.test(name)) {
+            if (/^(checkbox|radio)$/.test(DOM.getAttribute(elem, 'type')) && /^(radio|checked)$/.test(name)) {
                 if (name === "radio")
                     log("ms-duplex-radio已经更名为ms-duplex-checked")
                 name = "checked"
                 data.isChecked = true
+                
             }
             if (name === "bool") {
                 name = "boolean"
