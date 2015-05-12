4b824fc5d3f542097b963309a9bddb787ee6ba97
RubyLouvre <cheng19840218@gmail.com>
2015-05-12 14:25:19 +0800

    update mocha

--- test/if.js
+++ test/if.js
@@ -1,36 +1,33 @@
-var parse5 = require('parse5')
-var parser = new parse5.Parser();
-var serializer = new parse5.Serializer();
-//https://github.com/exolution/xCube/blob/master/XParser.js
 
-var avalon = require('../avalon')
-var vm = avalon.define({
-    $id: "test",
-    aaa: true,
-    bbb: false
+describe('测试ms-if', function () {
+    it("sync", function () {
+        var vm = avalon.define({
+            $id: "test",
+            aaa: true,
+            bbb: false
+        })
+        var text = heredoc(function () {
+            /*
+             <!DOCTYPE html>
+             <html ms-controller="test">
+             <head>
+             <title>测试if绑定的后端渲染</title>
+             </head>
+             <body>
+             <div ms-if="aaa">x</div>
+             <div ms-if="bbb"></div>
+             </body>
+             </html>
+             */
+        })
+        var dom = parser.parse(text)
+        avalon.scan(dom, vm)
+        var str = serializer.serialize(dom);
+        console.log(str)
+        expect(str.indexOf('<div>x</div>') !== -1).to.be(true)
+        expect(str.indexOf('<!--<div ms-if="bbb"></div>-->') !== -1).to.be(true)
     })
-    
-function heredoc(fn) {
-    return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').replace(/\*\/[^\/]+$/, '')
-}
-var text = heredoc(function(){
-    /*
-<!DOCTYPE html>
-<html ms-controller="test">
-    <head>
-        <title>测试if绑定的后端渲染</title>
-    </head>
-    <body>
-        <div ms-if="aaa"></div>
-        <div ms-if="bbb"></div>
-    </body>
-</html>
-     */
 })
-var dom = parser.parse(text)
-console.log(dom.childNodes[1].childNodes[2].childNodes[0])
-avalon.scan(dom, vm)
 
+ 
 
-var str = serializer.serialize(dom);
-console.log(str)
--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ global.heredoc = function (fn) {
     return fn.toString().replace(/^[^\/]+\/\*!?\s?/, '').replace(/\*\/[^\/]+$/, '')
 }
 
-var files = ["data"]
+var files = ["data","if"]
 files.forEach(function (val) {
     var filePath = path.resolve(process.cwd(), val)
     require(filePath)
