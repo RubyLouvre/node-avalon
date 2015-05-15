41ee1b50f28effede964ec3c924b12fd1eb490f9
qincheng <qincheng@unitedstack.com>
2015-05-16 00:19:06 +0800

    update

--- avalon.js
+++ avalon.js
@@ -5,7 +5,7 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon.js 1.43 built in 2015.5.15
+ avalon.js 1.43 built in 2015.5.16
  用于后端渲染
  */
 (function(){
@@ -1001,66 +1001,69 @@ var valHooks = {
         }
     }
 }
-function bindForBrowser(data){
+function bindForBrowser(data) {
     var attrName = 'ms-scan-331',
-        attrValue = ''
+            attrValue = ''
 
     // 提取 vmodels id
-    var array = data.vmodels.map(function(el){
+    var array = data.vmodels.map(function (el) {
         return el.$id
     })
 
     var element = data.element
 
-    if(DOM.nodeType(element) === 1){
+    if (DOM.nodeType(element) === 1) {
         // 如果是 Element 节点
-        
+
         // 提取 data 属性
         var props = 'name,param,priority,type,value',
-            options = {}
-        props.replace(rword,function(prop){
+                options = {}
+        props.replace(rword, function (prop) {
             options[prop] = data[prop]
         })
-        if(data.type === "include"){
+        if (data.type === "include") {
             options.template = data.template
         }
-        
+        if (data.type === "visible" ) {
+            options.isShow = data.isShow
+            options.inlineDisplay = data.inlineDisplay
+        }
         // 检测是否存在 ms-scan-noderebind
         if (DOM.hasAttribute(element, attrName)) {
             // 如果已有
             var newOptStr = JSON.stringify(options).replace(/"/ig, "'")
-            
+
             attrValue = DOM.getAttribute(element, attrName)
             attrValue = attrValue.replace('avalon.rebind([', 'avalon.rebind([' + newOptStr + ', ')
 
         } else {
             // 如果没有
-            attrValue = 'avalon.rebind('+ [JSON.stringify([options]), JSON.stringify(array)] +')';
+            attrValue = 'avalon.rebind(' + [JSON.stringify([options]), JSON.stringify(array)] + ')';
             // 将 Stringify 产生的双引号转换为单引号
             attrValue = attrValue.replace(/"/ig, "'");
         }
 
-        DOM.setAttribute(element, attrName , attrValue)
+        DOM.setAttribute(element, attrName, attrValue)
 
-    }else{
+    } else {
         // 如果是 Text 节点
-        
+
         // 提取 data 属性
         var props = 'expr,filters,type,value',
-            options = {}
-        props.replace(rword,function(prop){
+                options = {}
+        props.replace(rword, function (prop) {
             options[prop] = data[prop]
         })
 
         var newElement = DOM.createElement('span')
-            copy = DOM.cloneNode(element, true)
+        copy = DOM.cloneNode(element, true)
 
         newElement.childNodes.push(copy)
 
         // avalon.rebind
-        attrValue = 'avalon.rebind('+ [JSON.stringify([options]), JSON.stringify(array)] +')';
+        attrValue = 'avalon.rebind(' + [JSON.stringify([options]), JSON.stringify(array)] + ')';
         attrValue = attrValue.replace(/"/ig, "'");
-        DOM.setAttribute(newElement, attrName , attrValue)
+        DOM.setAttribute(newElement, attrName, attrValue)
 
         DOM.replaceChild(newElement, element)
     }
@@ -2710,24 +2713,25 @@ bindingHandlers.visible = function (data, vmodels) {
     if (style) { //如果用户在元素上设置了display
         var array = style.match(rdisplay) || []
         if (array[1]) {
-            data.display = array[1]
+            data.inlineDisplay = array[1]
         }
     }
     parseExprProxy(data.value, vmodels, data)
 }
 
 bindingExecutors.visible = function (val, elem, data) {
-    bindForBrowser(data)
     var style = DOM.getAttribute(elem, "style")
     if (val) { //如果要显示,如果在元素设置display:none,那么就去掉
-        if (style && data.display) {
-            var replaced = data.display === "none" ? "" : ["display:", data.display, ";"].join("")
+        if (style && data.inlineDisplay) {
+            var replaced = data.inlineDisplay === "none" ? "" : ["display:", data.inlineDisplay, ";"].join("")
             DOM.setAttribute(elem, "style", style.replace(rdisplay, replaced))
         }
     } else {  //如果要隐藏
         var cssText = !style ? "display:none;" : style.replace(rdisplay, "display:none;")
         DOM.setAttribute(elem, "style", cssText)
     }
+    data.isShow = val
+    bindForBrowser(data)
 }
 bindingHandlers["data"] = bindingHandlers["if"] = function (data, vmodels) {
     parseExprProxy(data.value, vmodels, data)
