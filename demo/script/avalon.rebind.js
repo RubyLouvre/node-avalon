3cdd8598cd827b6de61b1964c6f491a154a4fc1d
qincheng <qincheng@unitedstack.com>
2015-05-15 23:31:20 +0800

    update

--- demo/script/avalon.rebind.js
+++ demo/script/avalon.rebind.js
@@ -1,59 +1,56 @@
 /************************************************************************
  *               avalon.rebind node-avalon 绑定恢复                     *
  ************************************************************************/
-avalon.rebind = function (bindings, vmodelIds) {
-    var element = this,
-            vmodels = vmodelIds.map(function (id) {
-                return avalon.vmodels[id]
-            })
-    var getBindingCallback = function (elem, name, vmodels) {
-        var callback = elem.getAttribute(elem)
-        if (callback) {
-            for (var i = 0, vm; vm = vmodels[i++]; ) {
-                if (vm.hasOwnProperty(callback) && typeof vm[callback] === "function") {
-                    return vm[callback]
-                }
+new function () {
+    var bindingExecutors = avalon.bindingExecutors
+    var bindingHandlers = avalon.bindingHandlers
+    avalon.rebind = function (bindings, vmodelIds) {
+        var vmodels = vmodelIds.map(function (id) {
+            return avalon.vmodels[id]
+        })
+        for (var i = 0, data; data = bindings[i++]; ) {
+            var rebindFn = avalon.rebind[data.type]
+            console.log(typeof rebindFn)
+            if (typeof rebindFn === "function") {
+                rebindFn(data, vmodels, this)
             }
         }
     }
-    for (var i = 0, data; data = bindings[i++]; ) {
-        data.vmodels = vmodels
-        if (data.type === 'text') {
+    function noop() {
+    }
+    function injectBinding(name, data, vmodels) {
+        var fn = bindingExecutors[name]
+        bindingExecutors[name] = noop //防止刷新视图
+        bindingHandlers[name](data, vmodels)
+        bindingExecutors[name] = fn
+        data.handler = bindingExecutors[data.handlerName || name]
+    }
+    avalon.mix(avalon.rebind, {
+        text: function (data, vmodels, elem) {
             // 如果是插值表达式
             // 将 node-avalon 生成的 span 元素用文本节点替换
-            var parent = element.parentNode,
-                    textNode = element.childNodes[0].cloneNode(false)
-
-            parent.replaceChild(textNode, element)
+            var parent = elem.parentNode
+            var textNode = elem.childNodes[0].cloneNode(false)
+            parent.replaceChild(textNode, elem)
             data.element = textNode
-            avalon.bindingHandlers[data.type](data, vmodels)
-        } else if (data.type === 'include') {
-            var elem = data.element = this
-            data.includeRendered = getBindingCallback(elem, "data-include-rendered", vmodels)
-            data.includeLoaded = getBindingCallback(elem, "data-include-loaded", vmodels)
-            data.includeReplace = !!avalon(elem).data("includeReplace")
-            if (avalon(elem).data("includeCache")) {
-                data.templateCache = {}
-            }
-            data.startInclude = elem.firstChild
-            data.endInclude = elem.lastChild
-            data.handlerName = "attr"
+            injectBinding("text", data, vmodels)
+        },
+        include: function (data, vmodels, elem) {
+            data.element = elem
             if (data.template) {
                 var arr = data.template.split(" ")
                 var key = arr.shift()
                 var val = arr.join(' ')
                 avalon.templateCache[key] = val
             }
-            avalon.parseExprProxy(data.value, vmodels, data)
-        } else {
-            data.element = element
-            avalon.bindingHandlers[data.type](data, vmodels)
+            injectBinding("attr", data, vmodels)
+            elem.removeChild(elem.firstChild)
+            elem.removeChild(elem.lastChild)
+            data.startInclude = elem.firstChild
+            data.endInclude = elem.lastChild
         }
-
-        // 重新渲染  data.type 绑定
-
-
-
-
-    }
+    })
+    "title,alt,src,value,css,href".replace(avalon.rword, function (name) {
+        avalon.rebind[name] = avalon.rebind.attr
+    })
 }
