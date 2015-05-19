f52e2d496e00bd29f512f1d914e1cf55d33e1239
RubyLouvre <cheng19840218@gmail.com>
2015-05-19 17:32:13 +0800

    update

--- demo/script/html.js
+++ demo/script/html.js
@@ -1,10 +1,11 @@
 var vm = avalon.define({
     $id: 'test',
-    aaa: '含有<strong>加粗标签</strong>',
+    a: "xxx",
+    aaa: '含有<strong>{{a}}</strong>',
 
     change: function() {
         var randomNum = Math.random()
-
+        vm.a = new Date - 0
         vm.aaa = '含有<strong>加粗标签' + randomNum + '</strong>'
     }
 })
